01f91a2b77d45417a600ad535462afeb0fbfbd1f
samridh rana <samridhrana06@gmail.com>
2023-12-13 17:33:12 +0530

    Explicit and implicit type casting

---
+++ Fundamentals of java programing/typecasting.java
@@ -0,0 +1,26 @@
+public class typecasting {
+    public static void main(String[] args) {
+        // int value = 100;
+        // long longvalue = value;
+        // double doublevalue = longvalue;
+
+// implicit type casting
+
+        // System.out.println(value);
+        // System.out.println(longvalue);
+        // System.out.println(doublevalue);
+
+
+// Explicit type casting 
+        double doublevalue = 100.56;
+        int intvalue =(int) doublevalue;
+
+        System.out.println(doublevalue);
+        System.out.println(intvalue);
+
+
+
+
+    }
+    
+}
