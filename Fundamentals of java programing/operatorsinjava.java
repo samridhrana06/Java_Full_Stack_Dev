9658027122cd7a6c147f00ed5424d0b191e6b8ed
samridh rana <samridhrana06@gmail.com>
2023-12-13 17:51:51 +0530

    arthematic operators

---
+++ Fundamentals of java programing/operatorsinjava.java
@@ -0,0 +1,19 @@
+public class operatorsinjava {
+    public static void main(String[] args) {
+        
+
+        //operators
+        int number1 = 100;
+        int number2 = 15;
+
+        System.out.println("Addition: " + (number1+number2));
+        System.out.println("Substraction: " + (number1-number2));
+        System.out.println("multiplication: " + (number1*number2));
+        System.out.println("Division: " + (number1/number2));
+        System.out.println("Remainder: " + (number1%number2));
+        System.out.println("postincrement: " + (number1++));
+        System.out.println("preincrement: " + (++number2));
+
+    }
+    
+}
