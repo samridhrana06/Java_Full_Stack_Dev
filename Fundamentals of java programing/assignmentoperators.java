db0fbb00828b021c1006712d5b452b8ee5a74c94
samridh rana <samridhrana06@gmail.com>
2023-12-13 17:54:04 +0530

    Assignment operators

---
+++ Fundamentals of java programing/assignmentoperators.java
@@ -0,0 +1,15 @@
+public class assignmentoperators {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int num3;
+
+        num3 = num1;
+
+        System.out.println("num3 = num1: "+num3);
+        System.out.println("num3 = num1: "+num3);
+
+    }
+    
+}
